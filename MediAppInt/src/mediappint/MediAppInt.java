f7a45514fd108a91b536e6ab1f03dc3e62157dc4
Chris O'Brien <crazyirishman@gmail.com>
2014-04-15 19:06:51 -0400

    initial project creation

---
+++ MediAppInt/src/mediappint/MediAppInt.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mediappint;
+
+/**
+ *
+ * @author chriso
+ */
+public class MediAppInt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
