357352613912fad68586624136eb2e05b8ec4911
Chris O'Brien <crazyirishman@gmail.com>
2014-04-24 19:08:54 -0400

    Now with more getters and setters

--- MediAppInt/src/mediappint/Message.java
+++ MediAppInt/src/mediappint/Message.java
@@ -31,8 +31,79 @@ public class Message {
 
     public void setSendingApp(String SendingApp) {
         this.SendingApp = SendingApp;
-        System.out.println("Message.java.setSendingApp: " + SendingApp);
     }
-    
-    
+
+    public String getSendingFacility() {
+        return SendingFacility;
+    }
+
+    public void setSendingFacility(String SendingFacility) {
+        this.SendingFacility = SendingFacility;
+    }
+
+    public String getReceivingApp() {
+        return ReceivingApp;
+    }
+
+    public void setReceivingApp(String ReceivingApp) {
+        this.ReceivingApp = ReceivingApp;
+    }
+
+    public String getReceivingFacility() {
+        return ReceivingFacility;
+    }
+
+    public void setReceivingFacility(String ReceivingFacility) {
+        this.ReceivingFacility = ReceivingFacility;
+    }
+
+    public String getMsgDateTime() {
+        return MsgDateTime;
+    }
+
+    public void setMsgDateTime(String MsgDateTime) {
+        this.MsgDateTime = MsgDateTime;
+    }
+
+    public String getSecurity() {
+        return Security;
+    }
+
+    public void setSecurity(String Security) {
+        this.Security = Security;
+    }
+
+    public String getMsgType() {
+        return MsgType;
+    }
+
+    public void setMsgType(String MsgType) {
+        this.MsgType = MsgType;
+    }
+
+    public String getMsgCtrl() {
+        return MsgCtrl;
+    }
+
+    public void setMsgCtrl(String MsgCtrl) {
+        this.MsgCtrl = MsgCtrl;
+    }
+
+    public String getProcessingId() {
+        return ProcessingId;
+    }
+
+    public void setProcessingId(String ProcessingId) {
+        this.ProcessingId = ProcessingId;
+    }
+
+    public String getVersionId() {
+        return VersionId;
+    }
+
+    public void setVersionId(String VersionId) {
+        this.VersionId = VersionId;
+    }
+
+        
 }
